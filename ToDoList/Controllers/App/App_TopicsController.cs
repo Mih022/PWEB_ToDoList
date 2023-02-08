b68f7bf8fa82b48575c515ce934f57713a6ee3bf
mihael.ladic <mihael.ladic@guep.hr>
2023-02-08 22:51:47 +0100

    *Topics functionality

---
+++ ToDoList/Controllers/App/App_TopicsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Data;
+using ToDoList.Models.Database;
+
+namespace ToDoList.Controllers.App
+{
+    public class App_TopicsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public App_TopicsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Topics
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Topics.ToListAsync());
+        }
+
+        // GET: Topics/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Topics == null)
+            {
+                return NotFound();
+            }
+
+            var topic = await _context.Topics
+                .Include(p => p.ToDos)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (topic == null)
+            {
+                return NotFound();
+            }
+
+
+
+            return View(topic);
+        }
+    }
+}
